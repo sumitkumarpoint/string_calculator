e755bd22771577bcf4f97fccb6a040187ebdf594
Sumit Kumar <sumitkumarpoint@gmail.com>
2026-04-15 11:41:19 +0000

    feat: support multiple numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe StringCalculator do
   it 'returns 0 for empty string' do
     expect(StringCalculator.add('')).to eq(0)
   end
+
   it 'returns number when single number is provided' do
     expect(StringCalculator.add('1')).to eq(1)
+  end
+
+  it 'returns sum of two numbers' do
+    expect(StringCalculator.add('1,2')).to eq(3)
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,6 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i
+    numbers.split(",").map(&:to_i).sum
   end
 end
