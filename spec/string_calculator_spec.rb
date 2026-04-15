61a0f5be452dbae3c4edc314435ea67b120edf0c
Sumit Kumar <sumitkumarpoint@gmail.com>
2026-04-15 11:32:35 +0000

    feat: support single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,6 +3,9 @@ require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
   it 'returns 0 for empty string' do
-    expect(StringCalculator.add("")).to eq(0)
+    expect(StringCalculator.add('')).to eq(0)
+  end
+  it 'returns number when single number is provided' do
+    expect(StringCalculator.add('1')).to eq(1)
   end
 end
