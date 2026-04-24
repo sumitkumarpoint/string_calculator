cd33d2a73dd817c2cef3ae9d6c93dd22a51729e6
Sumit Kumar <sumitkumarpoint@gmail.com>
2026-04-24 08:28:18 +0000

    Fix: context grouping in specs

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,57 +2,71 @@
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
-  it 'returns 0 for empty string' do
-    expect(StringCalculator.add('')).to eq(0)
-  end
+  describe '.add' do
+    context 'when input is empty' do
+      it 'returns 0' do
+        expect(described_class.add('')).to eq(0)
+      end
+    end
 
-  it 'returns number when single number is provided' do
-    expect(StringCalculator.add('1')).to eq(1)
-  end
+    context 'when input has single number' do
+      it 'returns the number' do
+        expect(described_class.add('1')).to eq(1)
+      end
+    end
 
-  it 'returns sum of two numbers' do
-    expect(StringCalculator.add('1,2')).to eq(3)
-  end
+    context 'when input has multiple numbers' do
+      it 'returns sum of comma separated numbers' do
+        expect(described_class.add('1,2')).to eq(3)
+      end
 
-  it 'handles new lines between numbers' do
-    expect(StringCalculator.add("1\n2,3")).to eq(6)
-  end
+      it 'handles new lines between numbers' do
+        expect(described_class.add("1\n2,3")).to eq(6)
+      end
+    end
 
-  it 'supports custom delimiter' do
-    expect(StringCalculator.add("//;\n1;2")).to eq(3)
-  end
+    context 'when using custom delimiters' do
+      it 'supports single delimiter' do
+        expect(described_class.add("//;\n1;2")).to eq(3)
+      end
 
-  it 'raises exception for negative numbers' do
-    expect { StringCalculator.add('1,-2') }
-      .to raise_error('negatives not allowed -2')
-  end
+      it 'supports multiple delimiters' do
+        expect(described_class.add("//[*][%]\n1*2%3")).to eq(6)
+      end
 
-  it 'shows all negative numbers in exception' do
-    expect { StringCalculator.add('1,-2,-3') }
-      .to raise_error('negatives not allowed -2,-3')
-  end
+      it 'supports long delimiters' do
+        expect(described_class.add("//[***]\n1***2***3")).to eq(6)
+      end
+    end
 
-  it 'supports multiple delimiters' do
-    expect(StringCalculator.add("//[*][%]\n1*2%3")).to eq(6)
-  end
+    context 'when negative numbers are present' do
+      it 'raises error for one negative number' do
+        expect { described_class.add('1,-2') }
+          .to raise_error('negatives not allowed -2')
+      end
 
-  it 'supports long delimiters' do
-    expect(StringCalculator.add("//[***]\n1***2***3")).to eq(6)
-  end
+      it 'raises error showing all negative numbers' do
+        expect { described_class.add('1,-2,-3') }
+          .to raise_error('negatives not allowed -2,-3')
+      end
+    end
 
-  it 'Numbers bigger than 1000 should be ignored, so adding 2 + 1001 = 2' do
-    expect(StringCalculator.add("//[***]\n2***1001")).to eq(2)
-  end
+    context 'when numbers are greater than 1000' do
+      it 'ignores numbers greater than 1000' do
+        expect(described_class.add('2,1001')).to eq(2)
+      end
+    end
 
-  it 'raises error for leading delimiter' do
-    expect {
-      described_class.add(",1,2")
-    }.to raise_error(ArgumentError)
-  end
+    context 'when input is malformed' do
+      it 'raises error for leading delimiter' do
+        expect { described_class.add(',1,2') }
+          .to raise_error(ArgumentError)
+      end
 
-  it 'raises error for leading delimiter' do
-    expect {
-      described_class.add("1,,2")
-    }.to raise_error(ArgumentError)
+      it 'raises error for consecutive delimiters' do
+        expect { described_class.add('1,,2') }
+          .to raise_error(ArgumentError)
+      end
+    end
   end
 end
